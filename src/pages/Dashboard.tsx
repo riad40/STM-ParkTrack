ac89e82e157b35071976632701570b0798b410ff
riad40 <riadabdelaziz40@gmail.com>
2023-03-26 00:30:31 +0000

    added the filter by licensePlate

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,6 +27,27 @@ const Dashboard = (): JSX.Element => {
         setLogs(logs.filter((log: CarLog) => log._id !== id))
     }
 
+    // handle the filter by license plate
+    const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const filter = e.target.value.trim()
+
+        // if the filter is empty, get all the logs
+        if (!filter) {
+            const getLogsData = async () => {
+                const response = await getLogs()
+                setLogs(response)
+            }
+            getLogsData()
+            return
+        }
+
+        // filter the logs
+        const filteredLogs = logs.filter((log: CarLog) => {
+            return log.licensePlate.toLowerCase().includes(filter.toLowerCase())
+        })
+        setLogs(filteredLogs)
+    }
+
     // based on the user role, render the appropriate content
     if (auth.user?.roles?.includes("super admin")) {
         return (
@@ -38,6 +59,12 @@ const Dashboard = (): JSX.Element => {
                             <h1 className="text-2xl font-bold">
                                 The garage current state
                             </h1>
+                            <input
+                                type="text"
+                                placeholder="filter by license plate"
+                                onChange={handleFilter}
+                                className="border border-gray-300 rounded-md px-2 py-1"
+                            />
                             <Link to="/logs/create">
                                 <Button text="Add new Log" />
                             </Link>
